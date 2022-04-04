d7a8fbca723c71bbb11b3fbba4d0590e1b8a4a51
Shashank-73 <shashank220076@gmail.com>
2022-04-04 14:04:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let cursor=document.querySelector('.cursor');
+let holes=[...document.querySelectorAll('.hole')];
+let scoreElement=document.querySelector('.score span');
+let btn=document.querySelector('.btn button');
+
+let score=0;
+let audio=new Audio('assets/assets_smash.mp3');
+function go()
+{
+    const random=Math.floor(Math.random()* holes.length);
+    const hole=holes[random];
+    let timer=null;
+    const img=document.createElement('img');
+    img.classList.add('mole-img');
+    img.src='assets/mole.png';
+    img.addEventListener('click',()=>
+    {
+        score+=10;
+        scoreElement.textContent=score;
+        img.src='assets/mole-whacked.png';
+        audio.play();
+        clearTimeout(timer);
+        timer=setTimeout(()=>{
+            hole.removeChild(img);
+            go();
+        },500);
+    })
+    hole.appendChild(img);
+
+    timer=setTimeout(()=>{
+        hole.removeChild(img);
+        go();
+    },1500);
+}
+function stop()
+{
+    return;
+}
+btn.addEventListener('click',()=>
+{
+    if(btn.textContent=='Play')
+    {
+        btn.textContent="Stop";
+        go();
+    }
+})
+window.addEventListener('mousemove',event=>{
+    cursor.style.top=event.pageY+'px';
+    cursor.style.left=event.pageX+'px';
+});
+
+// active class functionality
+
+window.addEventListener('mousedown',()=>
+{
+    cursor.classList.add('active');
+})
+window.addEventListener('mouseup',()=>{
+    cursor.classList.remove('active');
+})
